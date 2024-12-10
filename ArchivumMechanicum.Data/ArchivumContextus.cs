5aeba3a5ba0caab9482299bf39844e8070b5bc9f
TTamas <tisza.tamas01@gmail.com>
2024-12-10 15:59:11 +0100

    Created Data project - Repository - Context

---
+++ ArchivumMechanicum.Data/ArchivumContextus.cs
@@ -0,0 +1,43 @@
+﻿using ArchivumMechanicum.Entities.Entity_Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivumMechanicum.Data
+{
+    public class ArchivumContextus : IdentityDbContext
+    {
+        public DbSet<Location> Territorium { get; set; }
+
+        public DbSet<Relic> Artefactum { get; set; }
+
+        public DbSet<Record> Archivum { get; set; }
+
+        public ArchivumContextus(DbContextOptions<ArchivumContextus> ctx)
+            : base(ctx)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Movie>()
+            //    .HasMany(m => m.Ratings)
+            //    .WithOne(r => r.Movie)
+            //    .HasForeignKey(r => r.MovieId)
+            //    .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Location>()
+                .HasMany(l => l.Relics)
+                .WithOne(r => r.Location)
+                .HasForeignKey(r => r.LocationIdentification)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ArchivumMechanicum.Data/Repositorium.cs
@@ -0,0 +1,61 @@
+﻿using ArchivumMechanicum.Entities.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivumMechanicum.Data
+{
+
+    public class Repositorium<T> where T : class, IdentityEntity
+    {
+        ArchivumContextus ctx;
+
+        public Repositorium(ArchivumContextus ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T entity)
+        {
+            ctx.Set<T>().Add(entity);
+            ctx.SaveChanges();
+        }
+
+        public T FindById(string id)
+        {
+            return ctx.Set<T>().First(t => t.Identification == id);
+        }
+
+        public void DeleteById(string id)
+        {
+            var entity = FindById(id);
+            ctx.Set<T>().Remove(entity);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            ctx.Set<T>().Remove(entity);
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Update(T entity)
+        {
+            var old = FindById(entity.Identification);
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(entity));
+            }
+            ctx.Set<T>().Update(old);
+            ctx.SaveChanges();
+        }
+    }
+}
