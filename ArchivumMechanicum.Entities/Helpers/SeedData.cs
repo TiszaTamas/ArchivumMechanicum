663ec95b10f7fa7377e828fb5cfad898b7b1e217
TTamas <tisza.tamas01@gmail.com>
2024-12-11 13:26:47 +0100

    Created experimental DataSeeder, to fill up tables if there is not any

---
+++ ArchivumMechanicum.Entities/Helpers/SeedData.cs
@@ -0,0 +1,17 @@
+﻿using ArchivumMechanicum.Entities.Entity_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivumMechanicum.Entities.Helpers
+{
+    public class SeedData
+    {
+        public List<Location> Locations { get; set; }
+        public List<Relic> Relics { get; set; }
+        public List<Record> Records { get; set; }
+    }
+
+}
---
+++ ArchivumMechanicum.Logic/Helpers/DatabaseSeeder.cs
@@ -0,0 +1,44 @@
+﻿using ArchivumMechanicum.Data;
+using ArchivumMechanicum.Entities.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivumMechanicum.Logic.Helpers
+{
+    public class DatabaseSeeder
+    {
+        private readonly ArchivumContext ctx;
+
+        public DatabaseSeeder(ArchivumContext context)
+        {
+            ctx = context;
+        }
+
+        public async Task PopulateDatabaseAsync()
+        {
+            if (!ctx.Locations.Any() && !ctx.Relics.Any() && !ctx.Records.Any())
+            {
+                var jsonData = System.IO.File.ReadAllText("seedData.json");
+
+                var seedData = JsonConvert.DeserializeObject<SeedData>(jsonData);
+
+                if (seedData != null)
+                {
+                    ctx.Locations.AddRange(seedData.Locations);
+                    ctx.Relics.AddRange(seedData.Relics);
+                    ctx.Records.AddRange(seedData.Records);
+                    ctx.SaveChanges();
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Database is already populated.");
+            }
+        }
+    }
+
+}
